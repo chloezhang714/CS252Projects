b85d0b9ae430ed3bf95dede76a07594bdf88b4fb
chloezhang714 <47944883+chloezhang714@users.noreply.github.com>
2023-02-26 00:23:44 -0500

    update transformation

--- Project2CheckIn/transformation.py
+++ Project2CheckIn/transformation.py
@@ -10,12 +10,11 @@ import palettable
 import analysis
 import data
 
-
 class Transformation(analysis.Analysis):
 
-    def __init__(self, orig_dataset, data=None):
-        orig_dataset = orig_dataset
-        self.data = data
+    def __init__(self, orig_dataset, data):
+        super().__init__(data)
+        self.orig_dataset = orig_dataset
         # '''Constructor for a Transformation object
 
         # Parameters:
@@ -34,66 +33,80 @@ class Transformation(analysis.Analysis):
         # pass
 
     def project(self, headers):
-        self.data = data(headers)
-        '''Project the original dataset onto the list of data variables specified by `headers`,
-        i.e. select a subset of the variables from the original dataset.
-        In other words, your goal is to populate the instance variable `self.data`.
+        self.data = data(headers = headers, data = self.dataset.select_data(headers))
+        
+        # '''Project the original dataset onto the list of data variables specified by `headers`,
+        # i.e. select a subset of the variables from the original dataset.
+        # In other words, your goal is to populate the instance variable `self.data`.
 
-        Parameters:
-        -----------
-        headers: Python list of str. len(headers) = `num_proj_vars`, usually 1-3 (inclusive), but
-            there could be more.
-            A list of headers (strings) specifying the feature to be projected onto each axis.
-            For example: if headers = ['hi', 'there', 'cs251'], then the data variables
-                'hi' becomes the 'x' variable,
-                'there' becomes the 'y' variable,
-                'cs251' becomes the 'z' variable.
-            The length of the list matches the number of dimensions onto which the dataset is
-            projected — having 'y' and 'z' variables is optional.
+        # Parameters:
+        # -----------
+        # headers: Python list of str. len(headers) = `num_proj_vars`, usually 1-3 (inclusive), but
+        #     there could be more.
+        #     A list of headers (strings) specifying the feature to be projected onto each axis.
+        #     For example: if headers = ['hi', 'there', 'cs251'], then the data variables
+        #         'hi' becomes the 'x' variable,
+        #         'there' becomes the 'y' variable,
+        #         'cs251' becomes the 'z' variable.
+        #     The length of the list matches the number of dimensions onto which the dataset is
+        #     projected — having 'y' and 'z' variables is optional.
 
-        TODO:
-        - Create a new `Data` object that you assign to `self.data` (project data onto the `headers`
-        variables). Determine and fill in 'valid' values for all the `Data` constructor
-        keyword arguments (except you dont need `filepath` because it is not relevant here).
-        '''
-        pass
+        # TODO:
+        # - Create a new `Data` object that you assign to `self.data` (project data onto the `headers`
+        # variables). Determine and fill in 'valid' values for all the `Data` constructor
+        # keyword arguments (except you dont need `filepath` because it is not relevant here).
+        # '''
+        # pass
 
     def get_data_homogeneous(self):
-        '''Helper method to get a version of the projected data array with an added homogeneous
-        coordinate. Useful for homogeneous transformations.
+        homogenized_data = np.append(self.data, np.array([np.ones(self.data.shape[0], dtype=int)]).T, axis=1)
+        return homogenized_data
+        # '''Helper method to get a version of the projected data array with an added homogeneous
+        # coordinate. Useful for homogeneous transformations.
 
-        Returns:
-        -----------
-        ndarray. shape=(N, num_proj_vars+1). The projected data array with an added 'fake variable'
-        column of ones on the right-hand side.
-            For example: If we have the data SAMPLE (just one row) in the projected data array:
-            [3.3, 5.0, 2.0], this sample would become [3.3, 5.0, 2.0, 1] in the returned array.
+        # Returns:
+        # -----------
+        # ndarray. shape=(N, num_proj_vars+1). The projected data array with an added 'fake variable'
+        # column of ones on the right-hand side.
+        #     For example: If we have the data SAMPLE (just one row) in the projected data array:
+        #     [3.3, 5.0, 2.0], this sample would become [3.3, 5.0, 2.0, 1] in the returned array.
 
-        NOTE:
-        - Do NOT update self.data with the homogenous coordinate.
-        '''
-        pass
+        # NOTE:
+        # - Do NOT update self.data with the homogenous coordinate.
+        # '''
+        # pass
 
     def translation_matrix(self, magnitudes):
-        ''' Make an M-dimensional homogeneous transformation matrix for translation,
-        where M is the number of features in the projected dataset.
+        translateTransform = np.eye(self.data.shape[1]+1, dtype=float)
+        for i in range(self.data.shape[1]):
+            translateTransform[i, self.data.shape[0]] = magnitudes[i]
+        return translateTransform
+        #print("translateTransform")
+        # ''' Make an M-dimensional homogeneous transformation matrix for translation,
+        # where M is the number of features in the projected dataset.
 
-        Parameters:
-        -----------
-        magnitudes: Python list of float.
-            Translate corresponding variables in `headers` (in the projected dataset) by these
-            amounts.
+        # Parameters:
+        # -----------
+        # magnitudes: Python list of float.
+        #     Translate corresponding variables in `headers` (in the projected dataset) by these
+        #     amounts.
 
-        Returns:
-        -----------
-        ndarray. shape=(num_proj_vars+1, num_proj_vars+1). The transformation matrix.
+        # Returns:
+        # -----------
+        # ndarray. shape=(num_proj_vars+1, num_proj_vars+1). The transformation matrix.
 
-        NOTE: This method just creates the translation matrix. It does NOT actually PERFORM the
-        translation!
-        '''
-        pass
+        # NOTE: This method just creates the translation matrix. It does NOT actually PERFORM the
+        # translation!
+        # '''
+        # pass
 
     def scale_matrix(self, magnitudes):
+        scaleTransform = np.eye(data.shape[1]+1)
+        # divide by the global range
+        for i in range(self.data.shape[1]):
+            scaleTransform[i, i] = magnitudes[i]
+        #print(scaleTransform)
+        return(scaleTransform)
         '''Make an M-dimensional homogeneous scaling matrix for scaling, where M is the number of
         variables in the projected dataset.
 
@@ -111,50 +124,58 @@ class Transformation(analysis.Analysis):
         pass
 
     def translate(self, magnitudes):
-        '''Translates the variables `headers` in projected dataset in corresponding amounts specified
-        by `magnitudes`.
+        translateTransform = self.translation_matrix(magnitudes = magnitudes)
+        data = (translateTransform @ self.get_data_homogeneous().T).T
+        data = np.delete(data, -1, axis = 1)
+        return data
+        # '''Translates the variables `headers` in projected dataset in corresponding amounts specified
+        # by `magnitudes`.
 
-        Parameters:
-        -----------
-        magnitudes: Python list of float.
-            Translate corresponding variables in `headers` (in the projected dataset) by these amounts.
+        # Parameters:
+        # -----------
+        # magnitudes: Python list of float.
+        #     Translate corresponding variables in `headers` (in the projected dataset) by these amounts.
 
-        Returns:
-        -----------
-        ndarray. shape=(N, num_proj_vars). The translated data (with all variables in the projected).
-            dataset. NOTE: There should be NO homogenous coordinate!
+        # Returns:
+        # -----------
+        # ndarray. shape=(N, num_proj_vars). The translated data (with all variables in the projected).
+        #     dataset. NOTE: There should be NO homogenous coordinate!
 
-        TODO:
-        - Use matrix multiplication to translate the projected dataset, as advertised above.
-        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
-        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
-        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a homogenous
-        coordinate!
-        '''
-        pass
+        # TODO:
+        # - Use matrix multiplication to translate the projected dataset, as advertised above.
+        # - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        # dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        # transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a homogenous
+        # coordinate!
+        # '''
+        # pass
 
     def scale(self, magnitudes):
-        '''Scales the variables `headers` in projected dataset in corresponding amounts specified
-        by `magnitudes`.
+        scaleTransform = self.scale_matrix(magnitudes = magnitudes)
+        data = (scaleTransform @ self.get_data_homogeneous().T).T
+        data = np.delete(data, -1, axis = 1)
+        return data
+        # '''Scales the variables `headers` in projected dataset in corresponding amounts specified
+        # by `magnitudes`.
 
-        Parameters:
-        -----------
-        magnitudes: Python list of float.
-            Scale corresponding variables in `headers` (in the projected dataset) by these amounts.
+        # Parameters:
+        # -----------
+        # magnitudes: Python list of float.
+        #     Scale corresponding variables in `headers` (in the projected dataset) by these amounts.
 
-        Returns:
-        -----------
-        ndarray. shape=(N, num_proj_vars). The scaled data (with all variables in the projected).
-            dataset. NOTE: There should be NO homogenous coordinate!
+        # Returns:
+        # -----------
+        # ndarray. shape=(N, num_proj_vars). The scaled data (with all variables in the projected).
+        #     dataset. NOTE: There should be NO homogenous coordinate!
 
-        TODO:
-        - Use matrix multiplication to scale the projected dataset, as advertised above.
-        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
-        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
-        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a
-        homogenous coordinate!
-        '''
-        pass
+        # TODO:
+        # - Use matrix multiplication to scale the projected dataset, as advertised above.
+        # - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        # dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        # transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a
+        # homogenous coordinate!
+        # '''
+        # pass
 
 
 #week 2
@@ -251,7 +272,13 @@ class Transformation(analysis.Analysis):
         pass
 
     def scatter_color(self, ind_var, dep_var, c_var, title=None):
-        '''Creates a 2D scatter plot with a color scale representing the 3rd dimension.
+        color_map = palettable.colorbrewer.sequential.Purples_9
+        x = self.data.select_data(ind_var)
+        y = self.data.select_data(dep_var)
+        z = self.data.select_data(c_var)
+        plt.scatter(x, y, c=z, s=75, cmap=color_map.mpl_colormap, edgecolor='black')
+        plt.title(title)
+        '''Creates a 2D scatter plot with a colo)r scale representing the 3rd dimension.
 
         Parameters:
         -----------
